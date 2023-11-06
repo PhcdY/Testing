0c0ce2207e27de9509ef3a3597ac7b505000ca2e
PhcdY <phucb2411@gmail.com>
2023-11-06 13:40:20 +0700

    yo

--- Testing.py
+++ Testing.py
@@ -1,13 +1,1 @@
-print("hello, world")
-#you are a bad programmer
-for i in range(0, 5):
-    print("what's up with your code man? It's complicated as hell")
-print("Hieu beo i")
-print("mmb")
-<<<<<<< Updated upstream
-=======
-for i in range(1,199):
-    print("mmb")
-
-    
->>>>>>> Stashed changes
+print("Remake")
